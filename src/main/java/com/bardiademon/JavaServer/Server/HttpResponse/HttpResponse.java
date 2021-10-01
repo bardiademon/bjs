6bf625d869b450f95450dc9d294516acc4da1d68
bardiademon <bardiademon@gmail.com>
2021-10-01 22:02:48 +0330

    feat(/Server/HttpResponse/HttpResponse.java): added createResponseFromClass(Object , int)

--- src/main/java/com/bardiademon/JavaServer/Server/HttpResponse/HttpResponse.java
+++ src/main/java/com/bardiademon/JavaServer/Server/HttpResponse/HttpResponse.java
@@ -5,6 +5,7 @@ import com.bardiademon.JavaServer.Server.Router;
 import com.bardiademon.JavaServer.bardiademon.Default;
 import com.bardiademon.JavaServer.bardiademon.Str;
 import com.bardiademon.JavaServer.bardiademon.Time;
+import com.google.gson.Gson;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -22,50 +23,8 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
-public final class HttpResponse
+public final class HttpResponse extends StatusCode
 {
-    public static final int SC_CONTINUE = 100;
-    public static final int SC_SWITCHING_PROTOCOLS = 101;
-    public static final int SC_OK = 200;
-    public static final int SC_CREATED = 201;
-    public static final int SC_ACCEPTED = 202;
-    public static final int SC_NON_AUTHORITATIVE_INFORMATION = 203;
-    public static final int SC_NO_CONTENT = 204;
-    public static final int SC_RESET_CONTENT = 205;
-    public static final int SC_PARTIAL_CONTENT = 206;
-    public static final int SC_MULTIPLE_CHOICES = 300;
-    public static final int SC_MOVED_PERMANENTLY = 301;
-    public static final int SC_MOVED_TEMPORARILY = 302;
-    public static final int SC_FOUND = 302;
-    public static final int SC_SEE_OTHER = 303;
-    public static final int SC_NOT_MODIFIED = 304;
-    public static final int SC_USE_PROXY = 305;
-    public static final int SC_TEMPORARY_REDIRECT = 307;
-    public static final int SC_BAD_REQUEST = 400;
-    public static final int SC_UNAUTHORIZED = 401;
-    public static final int SC_PAYMENT_REQUIRED = 402;
-    public static final int SC_FORBIDDEN = 403;
-    public static final int SC_NOT_FOUND = 404;
-    public static final int SC_METHOD_NOT_ALLOWED = 405;
-    public static final int SC_NOT_ACCEPTABLE = 406;
-    public static final int SC_PROXY_AUTHENTICATION_REQUIRED = 407;
-    public static final int SC_REQUEST_TIMEOUT = 408;
-    public static final int SC_CONFLICT = 409;
-    public static final int SC_GONE = 410;
-    public static final int SC_LENGTH_REQUIRED = 411;
-    public static final int SC_PRECONDITION_FAILED = 412;
-    public static final int SC_REQUEST_ENTITY_TOO_LARGE = 413;
-    public static final int SC_REQUEST_URI_TOO_LONG = 414;
-    public static final int SC_UNSUPPORTED_MEDIA_TYPE = 415;
-    public static final int SC_REQUESTED_RANGE_NOT_SATISFIABLE = 416;
-    public static final int SC_EXPECTATION_FAILED = 417;
-    public static final int SC_INTERNAL_SERVER_ERROR = 500;
-    public static final int SC_NOT_IMPLEMENTED = 501;
-    public static final int SC_BAD_GATEWAY = 502;
-    public static final int SC_SERVICE_UNAVAILABLE = 503;
-    public static final int SC_GATEWAY_TIMEOUT = 504;
-    public static final int SC_HTTP_VERSION_NOT_SUPPORTED = 505;
-
     private String text;
     private int statusCode = 200;
     private String contentType = HttpRequest.CT_TEXT_PLAIN;
@@ -160,6 +119,16 @@ public final class HttpResponse
         return response;
     }
 
+    public static HttpResponse createResponseFromClass (final Object obj)
+    {
+        return createResponseFromClass (obj , SC_OK);
+    }
+
+    public static HttpResponse createResponseFromClass (final Object obj , final int statusCode)
+    {
+        return createTextResponse (((new Gson ()).toJson (obj)) , statusCode , HttpRequest.CT_APP_JSON_OR_QL);
+    }
+
     public static HttpResponse createTextResponse (final Object text)
     {
         return createTextResponse (text , SC_OK);
@@ -167,7 +136,17 @@ public final class HttpResponse
 
     public static HttpResponse createTextResponse (final Object text , final int statusCode)
     {
-        return createTextResponse (((text == null) ? "null" : text.toString ()) , statusCode);
+        return createTextResponse (text , statusCode , HttpRequest.CT_TEXT_PLAIN);
+    }
+
+    public static HttpResponse createTextResponse (final Object text , final String contentType)
+    {
+        return createTextResponse (text , SC_OK , contentType);
+    }
+
+    public static HttpResponse createTextResponse (final Object text , final int statusCode , final String contentType)
+    {
+        return createTextResponse (((text == null) ? "null" : text.toString ()) , statusCode , contentType);
     }
 
     public static HttpResponse createTextResponse (final String text)
@@ -177,13 +156,23 @@ public final class HttpResponse
 
     public static HttpResponse createTextResponse (final String text , final int statusCode)
     {
+        return createTextResponse (text , statusCode , HttpRequest.CT_TEXT_PLAIN);
+    }
+
+    public static HttpResponse createTextResponse (final String text , final String contentType)
+    {
+        return createTextResponse (text , SC_OK , contentType);
+    }
+
+    public static HttpResponse createTextResponse (final String text , final int statusCode , final String contentType)
+    {
         final HttpResponse response = new HttpResponse ();
 
         response.setResponseType (HttpResponse.ResponseType.text);
         response.setStatusCode (statusCode);
         response.setCharset (StandardCharsets.UTF_8);
         response.setText (text);
-        response.setContentType (HttpRequest.CT_TEXT_PLAIN);
+        response.setContentType (contentType);
 
         return response;
     }
@@ -226,7 +215,6 @@ public final class HttpResponse
     public static HttpResponse createStreamResponse (final InputStream stream , final int statusCode)
     {
         return createStreamResponse (stream , statusCode , null);
-
     }
 
     public static HttpResponse createStreamResponse (final InputStream stream , final int statusCode , final String contentType)
